0c8411d1c0249477ca884a929d7dffe95654808e
Aukin Ivan <aukin2006@ya.ru>
2021-03-18 15:48:46 +0300

    Initial commit

---
+++ src/test/java/com/tz/convertercurrency/ConverterCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tz.convertercurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterCurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
